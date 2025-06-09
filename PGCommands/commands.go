b539a84e34a5983508c81733d26b32b2be3e7532
Mattias Makra <mattias@makra.org>
2025-06-09 11:21:03 +0200

    Track how long command ran for.

--- PGCommands/commands.go
+++ PGCommands/commands.go
@@ -6,7 +6,7 @@ type Command struct {
 }
 
 var commands = map[string]Command{
-	"Run UR":                 {Cmd: "cd ~/Developer/platform/source/controlling/Sources/Platform.Web; pnpm run ur", Description: "Update the platfor GraphQL and Relay"},
-	"Sync translations":      {Cmd: "cd ~/Developer/platform/source/controlling/Sources/Platform.Web; pnpm run sync-translations", Description: "Sync the translation files"},
-	"Update controlling api": {Cmd: "cd ~/Developer/platform/source/common/Sources; pnpm run update-controlling-api", Description: "Run autorst to generate Api client functions for Platform.Web"},
+	"Run UR":                 {Cmd: "time cd ~/Developer/platform/source/controlling/Sources/Platform.Web; pnpm run ur", Description: "Update the platfor GraphQL and Relay"},
+	"Sync translations":      {Cmd: "time cd ~/Developer/platform/source/controlling/Sources/Platform.Web; pnpm run sync-translations", Description: "Sync the translation files"},
+	"Update controlling api": {Cmd: "time cd ~/Developer/platform/source/common/Sources; pnpm run update-controlling-api", Description: "Run autorst to generate Api client functions for Platform.Web"},
 }
